dcbd70d9291a9118081aa44f66c5e8e65005e2b7
Aashispahari <aashishpahari156@gmail.com>
2023-06-25 15:57:21 +0545

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  var questionIndex = 0;
+  void answerQuestion() {
+    questionIndex = questionIndex + 1 ;
+    print(questionIndex);
+  }
+
+  Widget build(BuildContext context) {
+    var questions = ['what is your name', 'where do you live'];
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Starting Flutter'),
+        ),
+        body: Column(children: [
+          Text (questions[questionIndex]),
+          ElevatedButton(
+            onPressed: answerQuestion,
+            child: Text(questions[0]),
+          ),
+          ElevatedButton(
+            onPressed: () => print('Answer 2 choosen'),
+            child: Text('Answer 2'),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              // asdaff
+              print('Answer 3 choosen');
+            },
+            child: Text('Answer 3'),
+          ),
+        ]),
+      ),
+    );
+  }
+}
